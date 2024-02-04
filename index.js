ee136d0c59337b1adef4035b27b2fd260fffeec4
NPRojas <63315208+NPRojas@users.noreply.github.com>
2024-02-04 01:42:42 -0500

    delete unneeded  files

--- index.js
+++ index.js
@@ -1,40 +1,102 @@
-const myLibrary = ['The Lonely Giant', 'Life is Overwhemling', 'I Wish I Had Help', 'Life Sucks' , 'Will I Ever Make it Out', 'The Dark Woods'];
+const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
     //the constructor
-};
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
 
-function convertUserInput(event) {
+Book.prototype.isRead = function () {
+   
+}
+
+function initBtns() {
+    const addBookBtn = document.querySelector('.addBookBtn');
+
+    addBookBtn.addEventListener('click', () => {
+
+        const addBookForm = document.querySelector('.addBookForm');
+        addBookForm.showModal();
+   
+        const closeBtn = document.querySelector('.closeBtn');
+        closeBtn.onclick = () => addBookForm.close();
+
+        const bookForm = document.querySelector('.bookForm');
+        bookForm.addEventListener('submit', addBookToLibrary, true);
+    })
+}
+
+function addBookToLibrary(event) {
     event.preventDefault();
     // this is where the input will be turned into an object
-}
+    const titleInput = document.querySelector('#bookTitle');
+    const authorInput = document.querySelector('#author');
+    const pagesInput = document.querySelector('#numOfPages');
+    const readInput = document.querySelector('#read');
+    // book object gets added into the list
+    const book = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked );
+    insertObject(book, myLibrary);
 
-function addBook () {
-    const addBookForm = document.querySelector('.addBookForm');
-    addBookForm.showModal();
-   
-    const closeBtn = document.querySelector('.closeBtn');
-    closeBtn.onclick = () => addBookForm.close();
-
-    //this might need to be in a separate function 
+    console.log(book.read);
+    //resets form
     const bookForm = document.querySelector('.bookForm');
-    bookForm.addEventListener('submit', convertUserInput, true);
+    bookForm.reset();
+
+    displayBookInfo();
 }
 
-function addBookToLibrary() {
-    const addBookBtn = document.querySelector('.addBookBtn');
-    addBookBtn.onclick = addBook;
+// i can replace this with the push method
+function insertObject (object, list) {
+    let index = list.length;
+    list.splice(index, 0, object);
+}
 
+function displayBookInfo() {
     const article = document.querySelector('.article');
-    
-    for (const prod of myLibrary) {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        const title = document.createElement('h3');
-        title.textContent = prod;
-        card.appendChild(title);
-        article.appendChild(card);
-    }
-};
 
-addBookToLibrary();
+    const lastBookAdded = myLibrary.slice(-1)[0];
+   
+    const card = document.createElement('div');
+    card.classList.add('card');
+    // sets unique id to later target card for deletion
+    const bookID = myLibrary.findIndex((element) => element.title == lastBookAdded.title);
+    card.setAttribute('id', `${bookID}`);
+
+    const title = document.createElement('h3');
+    title.textContent = `${lastBookAdded.title}`;
+
+    const authorDisplay = document.createElement('p');
+    authorDisplay.textContent = `By ${lastBookAdded.author}`;
+
+    const pagesDisplay = document.createElement('p');
+    pagesDisplay.textContent =  `Pages: ${lastBookAdded.pages}`;
+    
+    const btn = document.createElement('button');
+    btn.textContent = 'Delete';
+    btn.classList.add('deleteBtn');
+ 
+    card.appendChild(title);
+    card.appendChild(authorDisplay);
+    card.appendChild(pagesDisplay);
+    card.appendChild(btn);
+    article.appendChild(card);
+
+    if (lastBookAdded.read) {
+        const readCheck = document.createElement('p');
+        readCheck.textContent = 'READ';
+        card.appendChild(readCheck);
+    };
+
+    btn.addEventListener('click', () => deleteBook(lastBookAdded));
+}
+
+function deleteBook(book) {
+    const bookID = myLibrary.findIndex((bookListed) => bookListed.title == book.title);
+    const card = document.getElementById(`${bookID}`);
+    card.remove();
+}
+
+initBtns();
+ 
--- test.js
+++
@@ -1,6 +0,0 @@
-//organize your functions 
-//Remove a book 
-
-
-//target the item in an array
-//delete an item in an array
